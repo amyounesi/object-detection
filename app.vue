7d19f59b378aa4276ff750b870e810ebb8ac339e
amirhosein younesi <hif.adv@gmail.com>
2023-02-02 00:44:55 +0330

    StartFetch

--- app.vue
+++ app.vue
@@ -34,12 +34,8 @@ import { GoogleMap, Marker,InfoWindow } from "vue3-google-map";
 
 export default defineComponent({
   components: { GoogleMap, Marker,InfoWindow },
-  setup() {
-    const markers = [
-      { lat: 40.689247, lng: -74.044502,popup:false },
-      { lat: 40.689248, lng: -74.044503,popup:false },
-      { lat: 40.689249, lng: -74.044504,popup:false }
-    ];
+  async setup() {
+    const { data: markers } = await useFetch('/api/position')
     function openInfoWindow(key){
       markers[key].popup=true;
     }
---
+++ server/api/position.get.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(() => {
+    return [
+        { lat: 40.689247, lng: -74.044502,popup:false },
+      { lat: 40.689248, lng: -74.044503,popup:false },
+      { lat: 40.689249, lng: -74.044504,popup:false }
+    ]
+})
